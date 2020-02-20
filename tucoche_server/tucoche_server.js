dd8ecf7047f92d79c00d7cfe006a6c0cff601c02
Unai Perez <unai_perez@cuatrovientos.es>
2020-02-20 09:34:46 +0100

    server

--- tucoche_server/tucoche_server.js
+++ tucoche_server/tucoche_server.js
@@ -1,17 +1,44 @@
 "use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const http = __importStar(require("http"));
-const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World!\n');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+class Car {
+    constructor(id, marca, modelo, image, puertas, precio) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.image = image;
+        this.puertas = puertas;
+        this.precio = precio;
+    }
+}
+const products = [
+    new Car("0", "First", "Car", "24.99", 3, 3424.99),
+    new Car("1", "Second", "Car", "24.99", 5, 2435.89),
+    new Car("2", "Third", "Car", "24.99", 5, 4524.99),
+    new Car("3", "Fourth", "Car", "24.99", 3, 2224.50)
+];
+function getCars() {
+    return products;
+}
+app.get('/', (req, res) => {
+    res.send('The URL of products is http://localhost:8000/products');
 });
-const port = 8000;
-server.listent(port);
-console.log('Listening on http://localhost:' + port);
+app.get('/products', (req, res) => {
+    res.json(getCars());
+});
+function getCarsById(productId) {
+    let p;
+    p = products.find((p) => p.id == productId);
+    return p;
+}
+app.get('/products/:id', (req, res) => {
+    res.json(getCarsById(req.params.id));
+});
+app.post('/postData', bodyParser.json(), (req, res) => {
+    console.log(res.json(req.body));
+});
+const server = app.listen(8000, "localhost", () => {
+    const { address, port } = server.address();
+    console.log('Listening on %s %s', address, port);
+});
--- tucoche_server/tucoche_server.ts
+++ tucoche_server/tucoche_server.ts
@@ -1,8 +1,46 @@
-import * as http from 'http';
-const server = http.createServer((request, response)=> {
- response.writeHead(200, {'Content-Type': 'text/plain'});
- response.end('Hello World!\n');
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+class Car {
+    constructor(
+        public id:string,
+        public marca:string,
+        public modelo:string,
+        public image:string,
+        public puertas:number,
+        public precio:number,
+    ){}
+}
+const products = [
+    new Car("0", "First","Car", "24.99",3,3424.99),
+    new Car("1", "Second","Car", "24.99",5,2435.89),
+    new Car("2", "Third","Car", "24.99",5,4524.99),
+    new Car("3", "Fourth","Car", "24.99",3,2224.50)
+]
+function getCars(): Car[] {
+    return products;
+}
+app.get('/', (req: any,res: any) =>{
+    res.send('The URL of products is http://localhost:8000/products');
 });
-const port = 8000;
-server.listent(port);
-console.log('Listening on http://localhost:' + port );
+app.get('/products', (req:any ,res:any) => {
+    res.json(getCars());
+});
+function getCarsById(productId: string): Car {
+    let p:any;
+    p = products.find((p:Car) => p.id == productId);
+    return p;
+}
+app.get('/products/:id', (req:any ,res:any) => {
+    res.json(getCarsById(req.params.id));
+});
+
+app.post('/postData', bodyParser.json(), (req:any, res:any) => {
+    console.log(res.json(req.body));
+});
+
+const server = app.listen(8000, "localhost", () => {
+    const {address, port} = server.address();
+    console.log('Listening on %s %s', address, port);
+});
